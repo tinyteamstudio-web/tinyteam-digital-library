74df6e3e8ce3f61dda3b7e8dcbc46981999e78fe
Luzien Joseph <tinyteamstudio@gmail.com>
2026-01-02 18:21:56 +0800

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,10 @@
 // assets/config.js
 window.TINYTEAM_CONFIG = {
-  SUBMISSION_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLScD-IqBpKkiTdrp1mfD3EUqqAEUfQLpE2O2ht6LhYX2DqpdqA/viewform",
+  // Always point to start.html (NOT Google Form directly)
+  SUBMISSION_FORM_URL: "start.html",
+
+  // Keep this for later (BuildFrame phase)
   PAYMENT_PROOF_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSdOa8FkqUfu8mXWuUgJtOHlUd7wu-DSnfMvpxbk0r8JkJD7GQ/viewform",
+
   SUPPORT_EMAIL: "smallsteps@tinyteamstudio.online"
 };
