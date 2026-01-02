75f341874efb320cfb386cf2b144aa640afad350
Luzien Joseph <tinyteamstudio@gmail.com>
2026-01-02 12:23:36 +0800

    Add configuration file for TinyTeam

---
+++ assets/config.js
@@ -0,0 +1,6 @@
+// assets/config.js
+window.TINYTEAM_CONFIG = {
+  SUBMISSION_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLScD-IqBpKkiTdrp1mfD3EUqqAEUfQLpE2O2ht6LhYX2DqpdqA/viewform",
+  PAYMENT_PROOF_FORM_URL: "PASTE_PAYMENT_PROOF_FORM_LINK_HERE",
+  SUPPORT_EMAIL: "smallsteps@tinyteamstudio.online"
+};
