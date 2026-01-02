d809684b12f37b872c891901da2973ac38a5d84c
Luzien Joseph <tinyteamstudio@gmail.com>
2026-01-02 12:34:19 +0800

    Update PAYMENT_PROOF_FORM_URL to new link

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 // assets/config.js
 window.TINYTEAM_CONFIG = {
   SUBMISSION_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLScD-IqBpKkiTdrp1mfD3EUqqAEUfQLpE2O2ht6LhYX2DqpdqA/viewform",
-  PAYMENT_PROOF_FORM_URL: "PASTE_PAYMENT_PROOF_FORM_LINK_HERE",
+  PAYMENT_PROOF_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSdOa8FkqUfu8mXWuUgJtOHlUd7wu-DSnfMvpxbk0r8JkJD7GQ/viewform",
   SUPPORT_EMAIL: "smallsteps@tinyteamstudio.online"
 };
